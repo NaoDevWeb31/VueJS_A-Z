588494c9a97a1be58a8160572dada66ab714d730
Naomi Mango <naomi.mango@gmail.com>
2021-11-24 20:26:21 +0100

    [MAJ] Développement Pro - Appel d'API

--- p3-dev_pro_composants_vue_cli/src/components/Contenu.vue
+++ p3-dev_pro_composants_vue_cli/src/components/Contenu.vue
@@ -1,71 +1,51 @@
 <template>
   <div class="container mt-5">
-      
-    <h1>{{ titre }}</h1>
+    <h1>Appel à une API</h1>
 
-        <div class="btn btn-outline-primary mr-2" @click="componentId = 'texte1'">Onglet 1</div>
-        <div class="btn btn-outline-primary" @click="componentId = 'texte2'">Onglet 2</div>
+    <div class="mt-4 mx-auto">
+      <img :src="urlImg" alt="Image de chat" class="img-thumbnail" />
+    </div>
 
-        <div class="onglets card mb-5">
-            <!-- Composants dynamiques -->
-            <component class="p-5" :is="componentId"></component>
-        </div>
-
-        <modale :revele="revele" :toggleModale="toggleModale"></modale>
-        <button @click="toggleModale" class="btn btn-success">Ouvre le modale</button>
-        
   </div>
 </template>
 
 <script>
-  import Texte1 from "./Texte1.vue";
-  import Texte2 from "./Texte2.vue";
-  import Modale from './Modale.vue';
+  import axios from "axios";
 
   export default {
     name: "Contenu",
     data() {
       return {
-        myArr: [
-          { titre: "Inception", date: 2010 },
-          { titre: "Avatar", date: 2009 },
-          { titre: "Seven", date: 1995 },
-        ],
-        txt: "Hello World",
-        titre: "Je suis le Titre",
-        toggle1: true,
-        toggle2: false,
-        componentId: "texte1",
-        revele: false,
+        urlImg: null,
       };
     },
+    mounted() {
+      this.getCatImg();
+      this.updateImage();
+    },
     methods: {
-      toggleOng1() {
-        this.toggle1 = true;
-        this.toggle2 = false;
+      getCatImg() {
+        axios
+          .get("https://api.thecatapi.com/v1/images/search")
+          .then((response) => {
+            this.urlImg = response.data[0].url;
+          });
       },
-      toggleOng2() {
-        this.toggle1 = false;
-        this.toggle2 = true;
+      updateImage() {
+        var newImage = new Image();
+        if (newImage.complete) {
+          newImage.src = this.urlImg;
+          this.urlImg = newImage.src = this.getCatImg();
+        }
+        setTimeout(this.updateImage, 10000);
       },
-        toggleModale(){
-            this.revele = !this.revele;
-        },
     },
-    components: {
-      "texte1": Texte1,
-      "texte2": Texte2,
-      "modale": Modale
-    },
+    components: {},
   };
 </script>
 
 <style>
   h1 {
     margin-top: 100px !important;
-  }
-
-  .onglets{
-      height: 300px;
   }
 </style>
--- p3-dev_pro_composants_vue_cli/src/components/Header.vue
+++ p3-dev_pro_composants_vue_cli/src/components/Header.vue
@@ -5,36 +5,12 @@
 </template>
 
 <script>
-  import { bus } from "@/main";
-
   export default {
     name: "Header",
     data() {
       return {
         titre: "Mon titre de base",
       };
-    },
-    beforeCreate() {
-      console.log("Il va être créé !");
-    },
-    created() {
-      console.log("Le composant est créé !");
-      // Données transmises de Liste.vue à Header.vue via le bus déclaré dans main.js
-      bus.$on("changeTitre", (data) => {
-        this.titre = data;
-      });
-    },
-    beforeMount() {
-      console.log("Il va être monté dans le DOM !");
-    },
-    mounted() {
-      console.log("Il est intégré dans le DOM !");
-    },
-    beforeUpdate() {
-      console.log("Il va se mettre à jour !");
-    },
-    updated() {
-      console.log("Le composant s'est mis à jour !");
     },
   };
 </script>
