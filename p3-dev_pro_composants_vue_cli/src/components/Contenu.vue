35d091a4b43b148845dec95f071ef28ae9108cb8
Naomi Mango <naomi.mango@gmail.com>
2021-11-24 13:59:12 +0100

    [MAJ] Développement Pro -  Créer et importer un composant

---
+++ p3-dev_pro_composants_vue_cli/src/components/Contenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ txt }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        txt: "Je suis le contenu",
+      };
+    },
+  };
+</script>
+
+<style></style>
