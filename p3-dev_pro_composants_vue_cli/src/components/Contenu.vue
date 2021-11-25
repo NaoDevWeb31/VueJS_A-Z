b06637acfddfe2dbf34b42b476ccaf726d0f7b35
Naomi Mango <naomi.mango@gmail.com>
2021-11-25 14:03:55 +0100

    [MAJ] Formulaires - Envoyer les données

--- p3-dev_pro_composants_vue_cli/src/components/Contenu.vue
+++ p3-dev_pro_composants_vue_cli/src/components/Contenu.vue
@@ -12,6 +12,7 @@
           type="text"
           id="prenom"
           class="form-control"
+          @input="toggleResult"
         />
       </div>
 
@@ -21,13 +22,14 @@
           v-model="formData.txt"
           id="txt"
           class="form-control"
+          @input="toggleResult"
         ></textarea>
       </div>
 
       <h2 class="h3 mt-3">Select Box</h2>
 
       <div class="form-group">
-        <select v-model="formData.select" class="form-control">
+        <select v-model="formData.select" class="form-control" @focus="toggleResult">
           <option v-for="(pays, index) in formData.listePays" :key="index">{{ pays }}</option>
         </select>
       </div>
@@ -35,24 +37,28 @@
       <h2 class="h3 mt-3">CheckBoxes</h2>
 
       <div class="form-check">
-          <input v-model="formData.checkFruits" value="Fraise" id="fraise" type="checkbox" class="form-check-input">
+          <input v-model="formData.checkFruits" value="Fraise" id="fraise" type="checkbox" class="form-check-input" @input="toggleResult">
           <label for="fraise" class="form-check-label">Fraise</label>
       </div>
 
       <div class="form-check">
-          <input v-model="formData.checkFruits" value="Pomme" id="pomme" type="checkbox" class="form-check-input">
+          <input v-model="formData.checkFruits" value="Pomme" id="pomme" type="checkbox" class="form-check-input" @input="toggleResult">
           <label for="pomme" class="form-check-label">Pomme</label>
       </div>
 
       <div class="form-check">
-          <input v-model="formData.checkFruits" value="Cerises" id="cerises" type="checkbox" class="form-check-input">
+          <input v-model="formData.checkFruits" value="Cerises" id="cerises" type="checkbox" class="form-check-input" @input="toggleResult">
           <label for="cerises" class="form-check-label">Cerises</label>
       </div>
 
+      <button @click.prevent="envoiForm" type="button" name="" id="" class="btn btn-primary mt-3 btn-lg btn-block">Envoyer les données</button>
 
-      <h2 class="card-header border mt-3">Résultats</h2>
+    </form>
 
-      <div class="card-body border p-3">
+    <div v-if="infoSubmit" class="card mt-3">
+      <h2 class="card-header">Résultats</h2>
+
+      <div class="card-body p-3">
         <p>Prénom : {{ formData.prenom }}</p>
         <p style="white-space: pre">Texte : {{ formData.txt }}</p>
 
@@ -63,8 +69,9 @@
 
         <p>Choix du select : {{ formData.select }}</p>
       </div>
+    </div>
 
-    </form>
+
 
   </div>
 </template>
@@ -80,10 +87,18 @@
           checkFruits: [],
           select: "",
           listePays: ["Russie", "Japon", "Canada", "Mexique"]
-        }
+        },
+        infoSubmit: false,
       };
     },
-    methods: {},
+    methods: {
+      envoiForm(){
+        this.infoSubmit = true;
+      },
+      toggleResult(){
+        this.infoSubmit = false;
+      }
+    },
     components: {},
   };
 </script>
