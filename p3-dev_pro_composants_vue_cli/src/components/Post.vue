7b79a465f8e578a2cb034c18ed996c59f203f7e6
Naomi Mango <naomi.mango@gmail.com>
2021-11-26 13:31:59 +0100

    [MAJ] Routing - Les paramètres des routes

---
+++ p3-dev_pro_composants_vue_cli/src/components/Post.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="container-fluid">
+
+
+    <div class="row mb-5">
+      <div class="col text-center">
+        <h1>Le Post numéro {{ id }}</h1>
+      </div>
+    </div>
+
+    <div class="row my-5 fill-page">
+      <div class="col">
+      </div>
+    </div>
+
+    <div class="row my-5 mx-auto">
+      <div class="col d-flex justify-content-center">
+        <div @click="goDebut" class="btn btn-warning w-50 py-2">Retour au début</div>
+      </div>
+    </div>
+
+
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "Post",
+    data() {
+        return {
+            id: this.$route.params.id,
+        }
+    },
+    methods: {
+      goDebut(){
+        this.$router.push("/");
+      }
+    }
+  };
+</script>
+
+<style>
+ .fill-page{
+   height: 40vh;
+ }
+</style>
