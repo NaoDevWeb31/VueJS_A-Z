ab3874b071fcd66323da9fb091be5dc1a851f5ff
Naomi Mango <naomi.mango@gmail.com>
2021-11-26 12:13:53 +0100

    [MAJ] Routing - Les liens

---
+++ p3-dev_pro_composants_vue_cli/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <ul>
+      <li><router-link to="/" exact>Accueil</router-link></li>
+      <li><router-link to="/page1">Page 1</router-link></li>
+      <li><router-link to="/page2">Page 2</router-link></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+  };
+</script>
+
+<style>
+  .router-link-active {
+    color: red;
+  }
+</style>
